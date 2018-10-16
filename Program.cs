d79865a174ca03996bd35e3d7e106b2d0c7d54f6
Guffy <guffy.nwa@gmail.com>
2018-10-16 11:21:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearning1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = false;
+            bool isTall = true;
+
+            if (isMale && !isTall)
+            {
+
+                Console.WriteLine("You are male and tall");
+            } else if (!isTall && !isMale)
+            {
+                Console.WriteLine("You are not male and nor are you tall.");
+            } else if (!isMale && isTall)   
+            {
+                Console.WriteLine("HEY TEST");
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
